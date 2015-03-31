98384fd7d0e03f69ba547bb43fa1720052d0816d
LaynePeng <appamail@hotmail.com>
2015-03-31 15:58:45 +0800

    1. Remove quote marks; 2. Remove break.
    Signed-off-by: LaynePeng <appamail@hotmail.com>

--- fragments/cfn-signal.sh
+++ fragments/cfn-signal.sh
@@ -4,7 +4,5 @@
 
 echo "notifying heat"
 curl -sf -X PUT -H 'Content-Type: application/json' \
-	--data-binary '{"Status": "SUCCESS",
-	"Reason": "Setup complete",
-	"Data": "OK", "UniqueId": "00000"}' \
+	--data-binary '{"Status": "SUCCESS", "Reason": "Setup complete", "Data": "OK", "UniqueId": "00000"}' \
 	"$WAIT_HANDLE"
--- fragments/install_cell.sh
+++ fragments/install_cell.sh
@@ -2,6 +2,7 @@
 
 set -e
 echo Start to install Lattice-Cell
+cat /var/lattice/setup/lattice-environment
 wget $LATTICE_TAR_URL --quiet -O /tmp/lattice.tgz
 tar -xf /tmp/lattice.tgz -C /tmp
 /tmp/lattice-build/common/install-common
