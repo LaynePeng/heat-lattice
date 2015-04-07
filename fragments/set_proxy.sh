599ef69bfae4fcf25f2a26490f4d40aaada45ef9
LaynePeng <appamail@hotmail.com>
2015-04-07 20:10:17 +0800

    1. Remove the proxy setting in Coordinator. Most of the time, coordinantor need not a proxy; 2. Fix the bug of setting proxy invalid after restart the machine.
    Signed-off-by: LaynePeng <appamail@hotmail.com>

--- fragments/set_proxy.sh
+++ fragments/set_proxy.sh
@@ -6,4 +6,8 @@ if [ "$PROXY" != "default" ]; then
 echo Start to Set Proxy: $PROXY
 
 export http_proxy=$PROXY
+export https_proxy=$PROXY
+
+echo export http_proxy=$PROXY >> /etc/profile
+echo export https_proxy=$PROXY >> /etc/profile
 fi
