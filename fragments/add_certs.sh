0c6c2b515a80952c92e74d5c3b030eb834b5c846
LaynePeng <appamail@hotmail.com>
2015-04-07 17:27:42 +0800

    Fix the bug, the ca not update after adding new crt.
    Signed-off-by: LaynePeng <appamail@hotmail.com>

--- fragments/add_certs.sh
+++ fragments/add_certs.sh
@@ -9,4 +9,5 @@ wget $CERTS_Files --quiet -O /tmp/certs.tgz
 mkdir /usr/share/ca-certificates/docker
 tar -xf /tmp/certs.tgz -C /usr/share/ca-certificates/docker
 cd /usr/share/ca-certificates && find ./docker/ -type f | sed 's/.\///' >> /etc/ca-certificates.conf
+update-ca-certificates
 fi
