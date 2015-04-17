7fcf166ca54c5dc464b03d0f391bbaf6a892639f
LaynePeng <appamail@hotmail.com>
2015-04-18 01:45:08 +0800

    Update to fit the latest lattice update: 1. the script path changed in Lattice build; 2. Coordinator changed to brain in latest build.
    Signed-off-by: LaynePeng <appamail@hotmail.com>

--- fragments/install_cell.sh
+++ fragments/install_cell.sh
@@ -5,5 +5,5 @@ echo Start to install Lattice-Cell
 cat /var/lattice/setup/lattice-environment
 wget $LATTICE_TAR_URL --quiet -O /tmp/lattice.tgz
 tar -xf /tmp/lattice.tgz -C /tmp
-/tmp/lattice-build/common/install-common
-/tmp/lattice-build/lattice-cell/install-lattice-cell
+/tmp/lattice-build/scripts/install-common
+/tmp/lattice-build/scripts/install-cell
--- fragments/install_coordinator.sh
+++ fragments/install_coordinator.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 set -e
-echo Start to install Lattice-Coordinator
+echo Start to install Lattice-Brain
 cat /var/lattice/setup/lattice-environment
 wget $LATTICE_TAR_URL --quiet -O /tmp/lattice.tgz
 tar -xf /tmp/lattice.tgz -C /tmp
-/tmp/lattice-build/common/install-common
-/tmp/lattice-build/coordinator/install-coordinator
+/tmp/lattice-build/scripts/install-common
+/tmp/lattice-build/scripts/install-brain
